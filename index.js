e2189ec7cf4bef57e5817d29902aa3dab783ed4d
Hawyar <hawyarxoshnaw@gmail.com>
2020-07-10 17:11:09 -0400

    minimal babel setup

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-require("dotenv").config();
+// esm config
+import "dotenv/config";
+import * as Validator from "@graphcms/validation";
 
 const { request } = require("graphql-request");
+
 const chalk = require("chalk");
+
+import testJson from "./test.json";
 async function main() {
   const endpoint = process.env.GRAPH_CMS_KEY;
+
+  // [[userinput]] from manifest.yml
+  const cmsCollection = `Posts`;
+  const path = `Blog`.toLowerCase();
+  console.log(chalk.cyan.bold(`GraphCMS data: ${cmsCollection}`));
+  console.log(chalk.greenBright.bold(`On path: ${path}`));
 
   const query = /* GraphQL */ `
     query MyQuery {
@@ -18,34 +29,34 @@ async function main() {
   const data = await request(endpoint, query);
 
   const type = JSON.stringify(data["__typename"]).replace(/^"(.*)"$/, "$1");
-  console.log(type);
+  // console.log(type);
 
-  // if (type !== "Query") {
-  //   console.log(
-  //     chalk.red(`Error:`),
-  //     `The first argument must be of type ${chalk.bold(
-  //       `Query`
-  //     )}, but you passed in type ${chalk.bold(type)}`
-  //   );
-  // }
+  if (type !== "Query") {
+    console.log(
+      chalk.red(`Error:`),
+      `The first argument must be of type ${chalk.bold(
+        `Query`
+      )}, but you passed in type ${chalk.bold(type)}`
+    );
+  }
 
   console.log(JSON.stringify(data, undefined, 2));
 
   var secondKey = Object.keys(data)[1]; //fetched the key at second index
 
-  console.log(secondKey);
+  // console.log(secondKey);
 
   // display before searching Gatsby
-  console.log(chalk.magenta.bold(`Inspecting Gatsby for GraphCMS queries`));
+  // console.log(chalk.magenta.bold(`Inspecting Gatsby for GraphCMS queries`));
 
-  // display before searching Gatsby
-  console.log(
-    chalk.magenta.green(
-      `Found GraphCMS query for ${chalk.bgWhiteBright.black.bold(
-        secondKey.charAt(0).toUpperCase().concat(secondKey.substr(1))
-      )} collection`
-    )
-  );
+  // // display before searching Gatsby
+  // console.log(
+  //   chalk.magenta.green(
+  //     `Found GraphCMS query for ${chalk.bgWhiteBright.black.bold(
+  //       secondKey.charAt(0).toUpperCase().concat(secondKey.substr(1))
+  //     )} collection`
+  //   )
+  // );
 }
 
 main().catch((error) => console.error(error));
